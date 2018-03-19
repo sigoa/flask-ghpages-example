17f046143076ff49d52669f5f1312a9a8f8be6ba
sigoa <si@goat.si>
2018-03-19 02:54:55 +0100

    crap

--- project/settings.py
+++ project/settings.py
@@ -2,11 +2,10 @@
 
 import os
 
-REPO_NAME = "flask-ghpages-example"  # Used for FREEZER_BASE_URL
+REPO_NAME = "webwonder"  # Used for FREEZER_BASE_URL
 DEBUG = True
 
-# Assumes the app is located in the same directory
-# where this file resides
+# Assumes the app is located in the same directory where this file resides
 APP_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def parent_dir(path):
@@ -14,14 +13,16 @@ def parent_dir(path):
     return os.path.abspath(os.path.join(path, os.pardir))
 
 PROJECT_ROOT = parent_dir(APP_DIR)
-# In order to deploy to Github pages, you must build the static files to
-# the project root
+# In order to deploy to Github pages, you must build the static files to the project root
+
 FREEZER_DESTINATION = PROJECT_ROOT
-# Since this is a repo page (not a Github user page),
-# we need to set the BASE_URL to the correct url as per GH Pages' standards
+# Since this is a repo page (not a Github user page), we need to set the BASE_URL to the correct url as per GH Pages' standards
+
 FREEZER_BASE_URL = "http://localhost/{0}".format(REPO_NAME)
-FREEZER_REMOVE_EXTRA_FILES = False  # IMPORTANT: If this is True, all app files
-                                    # will be deleted when you run the freezer
+FREEZER_REMOVE_EXTRA_FILES = False  # IMPORTANT: If this is True, all app files will be deleted when you run the freezer
+
+####   OK so what the fuck  is this flat shit in here
+
 FLATPAGES_MARKDOWN_EXTENSIONS = ['codehilite']
 FLATPAGES_ROOT = os.path.join(APP_DIR, 'pages')
 FLATPAGES_EXTENSION = '.md'
