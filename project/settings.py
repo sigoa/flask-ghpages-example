39fd59b509a015e19345cca3b4553e108c85b5da
sigoa <si@goat.si>
2018-03-19 02:57:08 +0100

    0

--- project/settings.py
+++ project/settings.py
@@ -14,13 +14,14 @@ def parent_dir(path):
 PROJECT_ROOT = parent_dir(APP_DIR)
 # In order to deploy to Github pages, you must build the static files to the project root
 
-FREEZER_DESTINATION = PROJECT_ROOT # Since this is a repo page (not a Github user page), we need to set the BASE_URL to the correct url as per GH Pages' standards
+FREEZER_DESTINATION = PROJECT_ROOT 
+# Since this is a repo page (not a Github user page), 
+# we need to set the BASE_URL to the correct url as per GH Pages' standards
 
 FREEZER_BASE_URL = "http://localhost/{0}".format(REPO_NAME)
 FREEZER_REMOVE_EXTRA_FILES = False  # IMPORTANT: If this is True, all app files will be deleted when you run the freezer
 
 
-####   OK so what the fuck  is this flat shit in here
-FLATPAGES_MARKDOWN_EXTENSIONS = ['codehilite']
+FLATPAGES_MARKDOWN_EXTENSIONS = ['codehilite']   ####   OK so what the fuck  is this flat shit in here
 FLATPAGES_ROOT = os.path.join(APP_DIR, 'pages')
 FLATPAGES_EXTENSION = '.md' 
